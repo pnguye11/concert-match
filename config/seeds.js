e44a89a57c2935e720ad47c45dbebf814b175133
Phuong Nathan Nguyen <pnguye11@gmail.com>
2016-06-27 16:13:47 -0700

    seed

--- config/seeds.js
+++ config/seeds.js
@@ -1,7 +1,18 @@
 var mongoose = require('mongoose');
 require('dotenv').load();
 
-var User = require('../models/user')
-mongoose.connect(process.env.DATABASE_URL);
+var User = require('../models/user');
 
-User.remove({}, function(err){console.log(err)});
+var users = [
+{
+  name : "Phuong Nguyen",
+  email: "turtl_unit_24@yahoo.com",
+  providerId: "57719a5cdefdcc9508b63f1e",
+  images: "https://i.ytimg.com/vi/e-ORhEE9VVg/maxresdefault.jpg"
+}];
+
+User.remove({}, function(err) {
+  console.log(err)
+});
+
+mongoose.connection.close();
